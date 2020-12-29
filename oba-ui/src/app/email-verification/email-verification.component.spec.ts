d888c511a2c338380cf0541b27806b2019c134e2
may <mpo@adorsys.com.ua>
2020-12-29 18:38:31 +0100

    fixed tests

--- oba-ui/src/app/email-verification/email-verification.component.spec.ts
+++ oba-ui/src/app/email-verification/email-verification.component.spec.ts
@@ -1,16 +1,38 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { EmailVerificationComponent } from './email-verification.component';
+import {EmailVerificationComponent} from './email-verification.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {ActivatedRoute, Router} from '@angular/router';
+import {of} from 'rxjs';
 
 describe('EmailVerificationComponent', () => {
   let component: EmailVerificationComponent;
   let fixture: ComponentFixture<EmailVerificationComponent>;
 
+  const router = {
+    navigate: jasmine.createSpy('navigate')
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EmailVerificationComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      declarations: [EmailVerificationComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({
+              verificationToken: 'ABC'
+            })
+          }
+        },
+        {provide: Router, useValue: router}
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
