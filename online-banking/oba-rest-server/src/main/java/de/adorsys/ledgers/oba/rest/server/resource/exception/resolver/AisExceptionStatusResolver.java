520bb19253904c5f5ad4f74165b53863154d98fb
Dmytro Mishchuk <dmi@adorsys.com.ua>
2021-02-01 14:39:56 +0200

    resolved pmd

--- online-banking/oba-rest-server/src/main/java/de/adorsys/ledgers/oba/rest/server/resource/exception/resolver/AisExceptionStatusResolver.java
+++ online-banking/oba-rest-server/src/main/java/de/adorsys/ledgers/oba/rest/server/resource/exception/resolver/AisExceptionStatusResolver.java
@@ -6,25 +6,23 @@ import org.springframework.http.HttpStatus;
 import java.util.EnumMap;
 import java.util.Map;
 
-import static de.adorsys.ledgers.oba.service.api.domain.exception.ObaErrorCode.NOT_FOUND;
 import static de.adorsys.ledgers.oba.service.api.domain.exception.ObaErrorCode.*;
-import static org.springframework.http.HttpStatus.*;
 
 public class AisExceptionStatusResolver {
     private static final Map<ObaErrorCode, HttpStatus> container = new EnumMap<>(ObaErrorCode.class);
 
     static {
         //400 Block
-        container.put(AIS_BAD_REQUEST, BAD_REQUEST);
+        container.put(AIS_BAD_REQUEST, HttpStatus.BAD_REQUEST);
 
         //401 Block
-        container.put(ACCESS_FORBIDDEN, FORBIDDEN);
+        container.put(ACCESS_FORBIDDEN, HttpStatus.FORBIDDEN);
         //404 Block
         container.put(NOT_FOUND, HttpStatus.NOT_FOUND);
 
         //500 Block
-        container.put(CONNECTION_ERROR, INTERNAL_SERVER_ERROR);
-        container.put(CONVERSION_EXCEPTION, INTERNAL_SERVER_ERROR);
+        container.put(CONNECTION_ERROR, HttpStatus.INTERNAL_SERVER_ERROR);
+        container.put(CONVERSION_EXCEPTION, HttpStatus.INTERNAL_SERVER_ERROR);
 
         container.put(AUTH_EXPIRED, HttpStatus.UNAUTHORIZED);
         container.put(LOGIN_FAILED, HttpStatus.UNAUTHORIZED);
